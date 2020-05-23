443fb982ae6f3b0febed0eb48b8a47a0feb87d7c
AlexIsOK <56567712+AlexIsOK@users.noreply.github.com>
2020-05-23 13:38:32 -0700

    Works, will make more mods later.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 
 const discord = require("discord.js");
 const client = new discord.Client();
-const fs = require("fs");
+const pbip = require("public-ip");
 
-if(!fs.existsSync("auth.json"))
-    fs.writeFileSync("./auth.json", "")
+const {existsSync, readFileSync, writeFileSync} = require("fs");
+
+if(!existsSync("auth.json")) {
+    writeFileSync("./auth.json", "{\n\"token\":\"TOKEN\"\n}");
+    console.log("Please enter your token in auth.json and re-run.");
+    process.exit(1);
+}
+
+//get the token from auth.json
+const tkn = (JSON.parse(readFileSync("auth.json"))).token;
+const prefix = (JSON.parse(readFileSync("auth.json"))).prefix;
+
+
+client.on("message", msg => {
+    if(msg.content === prefix + "ip") {
+        pbip.v4().then(ip => {
+            msg.reply(ip);
+        });
+    }
+});
+
+client.login(tkn);
